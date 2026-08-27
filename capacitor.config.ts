595893c382480f1d7cf1c08a5922fc8d4b144180
Daniel-Unegbu <odesigounegbu3@gmail.com>
2026-08-27 20:24:32 +0100

    dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.devoted.app',
   appName: 'Devoted',
-  webDir: 'out'
+  webDir: 'dist'
 };
 
 export default config;
