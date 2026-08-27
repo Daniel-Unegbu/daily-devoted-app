2e6b1dd9f78a00e7c58c14fad7a5a95265616324
Daniel-Unegbu <odesigounegbu3@gmail.com>
2026-08-27 20:20:32 +0100

    onic

---
+++ android/app/src/main/java/com/devoted/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.devoted.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.devoted.app',
+  appName: 'Devoted',
+  webDir: 'out'
+};
+
+export default config;
